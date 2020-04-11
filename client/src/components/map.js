8fac607ecb61aad2f018cbb1a66ec2e61b17e82d
DanielYu2004 <DanielYu2004@users.noreply.github.com>
2020-04-11 19:41:24 -0400

    map data

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-//import Hos from './data/hospitals.json'
+import Hos from '../data/hospitals.json'
+import { Button } from 'rsuite';
 
 
 const google = window.google
@@ -11,27 +12,14 @@ class MapContainer extends Component {
     componentDidMount(){
 
         var heatmapData = [
-            new google.maps.LatLng(37.782, -122.447),
-            new google.maps.LatLng(37.782, -122.445),
-            new google.maps.LatLng(37.782, -122.443),
-            new google.maps.LatLng(37.782, -122.441),
-            new google.maps.LatLng(37.782, -122.439),
-            new google.maps.LatLng(37.782, -122.437),
-            new google.maps.LatLng(37.782, -122.435),
-            new google.maps.LatLng(37.785, -122.447),
-            new google.maps.LatLng(37.785, -122.445),
-            new google.maps.LatLng(37.785, -122.443),
-            new google.maps.LatLng(37.785, -122.441),
-            new google.maps.LatLng(37.785, -122.439),
-            new google.maps.LatLng(37.785, -122.437),
-            new google.maps.LatLng(37.785, -122.435)
+            {location: new google.maps.LatLng(40.7099, -74.0048), weight: 50}
         ];
 
 
 
         var map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 37.0902, lng: -95.7129},
-            zoom: 4
+            center: {lat: 40.7099, lng: -74.0048},
+            zoom: 11
         });
 
         var heatmap = new google.maps.visualization.HeatmapLayer({
@@ -43,39 +31,39 @@ class MapContainer extends Component {
 
 
     }
-    // addMarker(){
-    //     if (this.state.hospitals == true){
-    //         console.log(this.state.hospitals)
-    //         var markerList = []
+    addMarker(){
+        if (this.state.hospitals == true){
+            console.log(this.state.hospitals)
+            var markerList = []
 
-    //         Hos.features.map((value, index) => {
-    //             markerList.push(new google.maps.Marker({
-    //                 position: {lat: value.attributes.LATITUDE, lng: value.attributes.LONGITUDE},
-    //                 map: this.state.map,
-    //             }))
-    //             markerList[markerList.length - 1].setMap(this.state.map)
-    //         })
-    //         this.setState(() => {
-    //             return {
-    //                 "markerList" : markerList,
-    //                 "hospitals" : false
-    //             }
-    //         });
-    //     }
-    //     else{
+            Hos.features.map((value, index) => {
+                markerList.push(new google.maps.Marker({
+                    position: {lat: value.attributes.LATITUDE, lng: value.attributes.LONGITUDE},
+                    map: this.state.map,
+                }))
+                markerList[markerList.length - 1].setMap(this.state.map)
+            })
+            this.setState(() => {
+                return {
+                    "markerList" : markerList,
+                    "hospitals" : false
+                }
+            });
+        }
+        else{
 
-    //         for (var i = 0; i < this.state.markerList.length; i++) {
-    //             this.state.markerList[i].setMap(null);
-    //         }
-    //         this.setState({hospitals : true})
+            for (var i = 0; i < this.state.markerList.length; i++) {
+                this.state.markerList[i].setMap(null);
+            }
+            this.setState({hospitals : true})
 
-    //     }
-    // }
+        }
+    }
   render() {
     return (
       <div style={{ height: '50vh', width: '50%' }}>
         <div id="map"></div>
-        <button onClick={() => null}>BUTTOn</button>
+        <Button appearance="primary" onClick={() => this.addMarker()} active>Toggle Hospitals</Button>
       </div>
     )
   }
