75e6d3f0f0ca0c8d5e88e90ee7be2176563854a6
anshgupta1234 <ansh.guptaca.2004@gmail.com>
2020-04-11 20:31:47 -0700

    update

--- client/src/components/requests.js
+++ client/src/components/requests.js
@@ -19,7 +19,14 @@ export default class Requests extends React.Component {
             "body": JSON.stringify({
                 "hospitalId": "252sdfgsdgv"
             })
-        }).then(res => res.json()).then(res => console.log(res))
+        }).then(res => res.json()).then(res => {
+            console.log(res)
+            this.addPending(res.data)
+        })
+    }
+
+    addPending = data => {
+        this.setState({ pending: [...this.state.pending, ...data]})
     }
 
     render() { 
@@ -27,8 +34,14 @@ export default class Requests extends React.Component {
             <div style = {{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', padding: '50px' }}>
                 <h4>Have any donors given you medical supplies?</h4>
                 <Button style={{ margin: '10px' }} appearance="primary" size="lg" onClick={() => this.checkPending()}>Check for Pending Donation Requests</Button>
-                <div style={{ height: '600%', overflowY: 'scroll' }}>
+                <div style={{ height: '60%', width: '100%', display: 'flex', alignItems: 'flex-start', flexDirection: 'column' }}>
+                    {
+                        this.state.pending.map((req, i) => (
+                            <div style={{ height: '150px', width: '100%', margin: '10px', padding: '10px', backgr: 'green', borderWidth: 1, borderColor: 'black' }}>
 
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
          );
