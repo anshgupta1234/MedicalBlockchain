c9f1abb5817dff35e3573c2e1fddedcbe35dc19c
anshgupta1234 <ansh.guptaca.2004@gmail.com>
2020-04-12 03:28:39 -0700

    supply works

--- client/src/components/donate.js
+++ client/src/components/donate.js
@@ -5,23 +5,37 @@ import { Button, ButtonToolbar, RadioGroup, Radio} from 'rsuite';
 export default class Donate extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { formValue: {} }
     }
 
-    submit(){
-        //const id = 
+    submit = () => {
+        const { id, amount } = this.state.formValue
+        fetch("http://localhost:8000/new", {
+            "method": "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            "body": JSON.stringify({
+                "hospitalId": id,
+                amount,
+                type: this.state.type.toLowerCase()
+            })
+        }).then(res => res.json()).then(res => {
+            console.log(res)
+            
+        })
     }
 
     render() { 
         return ( 
             <div style={{display: "flex", justifyContent : "center", alignItems : "center", height: "100%", width: "100%"}}>
-            <Form>
+            <Form onChange={formValue => this.setState({ formValue })}>
                 <FormGroup>
                     <ControlLabel>Hospital Id</ControlLabel>
-                    <FormControl name="name" />
+                    <FormControl name="id" />
                     <HelpBlock>Required</HelpBlock>
                 </FormGroup>
-                <RadioGroup>
+                <RadioGroup name='type' onChange={value => this.setState({ type: value })}>
                     <Radio value="Gloves">Gloves</Radio>
                     <Radio value="Ventilators">Ventilators</Radio>
                     <Radio value="Medical Masks">Medical Masks</Radio>
@@ -32,8 +46,8 @@ export default class Donate extends React.Component {
                     <FormControl
                     accepter={Slider}
                     min={0}
-                    max={100000}
-                    name="Amount"
+                    max={1000}
+                    name="amount"
                     label="Level"
                     style={{ width: 300, margin: '10px 0' }}
                     />
